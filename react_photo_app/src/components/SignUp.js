40015ccccf6663740c425759405e49d22872244e
dev-augustin <dev.augustin00@gmail.com>
2020-04-20 23:23:26 -0500

    initial commit

---
+++ react_photo_app/src/components/SignUp.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Signup extends Component {
+    render() {
+        return (
+            <div>
+               
+                <h3>New User - Signup</h3>
+                <form>
+                    <label>  Username:  </label>
+                    <input type="" name="" value={}/>
+                    <label>Email: </label>
+                    <input type="" name="" value={}/>
+                    <label>  Password  </label>
+                    <input type="" name="" value={}/>
+                    <input type="submit" name="submit" />
+                 
+                </form>
+            </div>
+        )
+    }
+}
