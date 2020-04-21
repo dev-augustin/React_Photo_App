f06f23d90b4631eff63bd3e6e2ae722bcdd48a15
dev-augustin <dev.augustin00@gmail.com>
2020-04-21 10:07:33 -0500

    added code for error message to display in react-app

--- react_photo_app/src/components/Home.js
+++ react_photo_app/src/components/Home.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import firebaseConfig from '../firebaseConfig'
 
 export default class Home extends Component {
+    constructor(props){
+        super(props);
+        this.logout=this.logout.bind(this);
+        
+    }
+
+    logout =(event) =>{
+        event.preventDefault();
+        firebaseConfig.auth().signOut()
+        .then((error) => {
+            var errorMessage=error.message;
+            console.log(error);
+            console.log(error.message);
+            alert(errorMessage);
+        });
+    }
     render() {
         return (
             <div>
                 <h1> Welcome to Home screen</h1>
+
+                <button type="button" onClick={this.logout}>Logout</button>
             </div>
         )
     }
