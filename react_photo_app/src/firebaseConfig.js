9775fad2fac17b0c7fd8d53bbaf18540f714ef77
dev-augustin <dev.augustin00@gmail.com>
2020-04-21 01:00:10 -0500

    resolved initializeApp is not a function error

--- react_photo_app/src/firebaseConfig.js
+++ react_photo_app/src/firebaseConfig.js
@@ -1,7 +1,9 @@
-import firebase from 'firebase';
+// import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firebase-auth'
 
 //configuration settings from firebase console for the firebase project
-const config = {
+const firebaseSetting= firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -9,12 +11,14 @@ const config = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
+  });
 
   //initializing firebase
-  const firebaseConfig= firebase.inititalizeApp(config);
-  console.log(firebaseConfig.name);
+  // const firebaseSetting= firebase.inititalizeApp(config);
+  console.log(firebaseSetting.name);
   //exporting 
-  export default firebaseConfig;
+  export default firebaseSetting;
 
-  //Resource: Firebase docs https://firebase.google.com/docs
+  //Resource: Firebase docs 
+  //https://firebase.google.com/docs
+  //https://firebase.google.com/docs/reference/js/firebase#initializeapp
