9ac05c594bc395557d6398baafff709a85e2b1bc
dev-augustin <dev.augustin00@gmail.com>
2020-04-21 01:13:49 -0500

    added handleChange function

--- react_photo_app/src/components/Login.js
+++ react_photo_app/src/components/Login.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import  firebaseConfig from '../firebaseConfig';
 
 export default class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            Email: "",
+            Password: ""
+        }
+        this.handleChange=this.handleChange.bind(this);
+        this.login=this.login.bind(this);
+    }
+     handleChange(event){
+         this.setState({[event.target.name] : event.target.value})
+     }
+     
+     login(event){
+         
+     }
     render() {
         return (
             <div>
@@ -8,10 +25,10 @@ export default class Login extends Component {
                 <h3>Login</h3>
                 <form>
                     <label>Email: </label>
-                    <input type="" name="" />
+                    <input type="email" name="Email" value={this.state.Email} onChange={this.handleChange} />
                     <label>  Password  </label>
-                    <input type="" name="" />
-                    <input type="submit" name="submit" />
+                    <input type="password" name="Password" value={this.state.password} onChange={this.handleChange}/>
+                    <button type="button"  onClick={this.login}> Login </button>
                  
                 </form>
             </div>
