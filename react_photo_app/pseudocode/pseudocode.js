f7bcf1e5eba3763bf88a04610d0517c45eb117d1
dev-augustin <dev.augustin00@gmail.com>
2020-04-20 19:38:13 -0500

    initial pseudocode

--- react_photo_app/pseudocode/pseudocode.js
+++ react_photo_app/pseudocode/pseudocode.js
@@ -6,6 +6,11 @@
 // User Profile detail with usage
 // Deleting Comments, photos and Users
 
-//
+//1. Create React App
+//2. create a project in Firebase
+//3. Select the Authentication method - Google
+//4. Copy the config details
+//5. Install Firebase - npm install --save firebase
+//6. Under src -> create firebaseConfig file
 
 
