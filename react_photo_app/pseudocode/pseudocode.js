1e60793934f5e3117dca41acf256d26f4ac64f42
dev-augustin <dev.augustin00@gmail.com>
2020-04-20 22:40:08 -0500

    initial pseudocode

---
+++ react_photo_app/pseudocode/pseudocode.js
@@ -0,0 +1,10 @@
+//Create React App
+//Go to Firebase > create a project
+//In Firebase > go to Authentication > select google 
+//In webApp, get the config settings for firebase
+//In react App, create .env file and add it to .gitignoore
+//Then add config settings to .env file
+//In App.js do the firebase authentication if user is logged_in or logged_out
+//If logged in Home page should be displayed
+//Else Login screen
+//If new user, signup a=form
