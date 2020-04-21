7a540cdaac1de42ae341c2ebb346c27ba4715f85
dev-augustin <dev.augustin00@gmail.com>
2020-04-21 01:51:47 -0500

    initial form

--- react_photo_app/src/components/SignUp.js
+++ react_photo_app/src/components/SignUp.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 export default class Signup extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            Username :"",
+            Emai: "",
+            Password : ""
+        }
+    }
     render() {
         return (
             <div>
@@ -8,12 +16,12 @@ export default class Signup extends Component {
                 <h3>New User - Signup</h3>
                 <form>
                     <label>  Username:  </label>
-                    <input type="" name="" value={}/>
+                    <input type="text" name="" value={this.state.Username}/>
                     <label>Email: </label>
-                    <input type="" name="" value={}/>
+                    <input type="Email" name="" value={this.state.Email}/>
                     <label>  Password  </label>
-                    <input type="" name="" value={}/>
-                    <input type="submit" name="submit" />
+                    <input type="Paswwrod" name="" value={this.state.Password}/>
+                    <button>Signup</button>
                  
                 </form>
             </div>
