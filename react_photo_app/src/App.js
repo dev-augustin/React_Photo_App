769707cd54fc1d178f515ad5fd9db1149f33d38f
dev-augustin <dev.augustin00@gmail.com>
2020-04-21 01:00:40 -0500

    update code with authstate

--- react_photo_app/src/App.js
+++ react_photo_app/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import firebaseConfig from './firebaseConfig';
+import 'firebase/firebase-auth'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./components/Home"
+import Login from "./components/Login"
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      user:""
+    }
+  }
+componentDidMount(){
+  this.authState();
+}
+  //on AuthSgateChanged method to check sign-in state of user
+  authState(){
+     firebaseConfig.auth().onAuthStateChanged((user) =>{
+        console.log("user: ", user);
+        if(user){
+          this.setState({user});
+        }
+        else{
+          this.setState({user:""});
+        }
+     });
+  }
+
+  render() {
+    return (
+      <div>
+        {/* If user ,home component else Login components*/}
+       {this.state.user ? (<Home/>) : (<Login/>)}
+      </div>
+    )
+  }
 }
 
-export default App;
