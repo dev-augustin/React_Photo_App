51276bd98f099e11fb6b81e482e901deb4a9004a
dev-augustin <dev.augustin00@gmail.com>
2020-04-20 23:22:10 -0500

    initial commit

---
+++ react_photo_app/src/components/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+               
+                <h3>Login</h3>
+                <form>
+                    <label>Email: </label>
+                    <input type="" name="" value={}/>
+                    <label>  Password  </label>
+                    <input type="" name="" value={}/>
+                    <input type="submit" name="submit" />
+                 
+                </form>
+            </div>
+        )
+    }
+}
