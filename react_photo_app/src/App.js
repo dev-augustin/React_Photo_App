69f33b9f8b42e4041e79ef29f16961a4985b32c0
dev-augustin <dev.augustin00@gmail.com>
2020-04-22 10:53:44 -0500

    added database but not working

--- react_photo_app/src/App.js
+++ react_photo_app/src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import firebaseConfig from './firebaseConfig';
 import 'firebase/firebase-auth'
-
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Home from "./components/Home"
 import Login from "./components/Login"
+import Bookmarks from './components/Bookmarks';
+import ProfilePage from './components/ProfilePage';
+
 
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      user:""
+      user:"",
+      Email:""
     }
   }
 componentDidMount(){
@@ -22,18 +26,36 @@ componentDidMount(){
         console.log("user: ", user);
         if(user){
           this.setState({user});
+          console.log(user.uid);
+          console.log(user.email)
+          // getUserData(user.uid);
         }
         else{
           this.setState({user:""});
         }
      });
   }
-
+// getUserData(uid){
+//   firebaseConfig.database().ref('users/' + uid).once('value', snap =>{
+//     console.log(snap.val())
+//   })
+// }
   render() {
     return (
       <div>
-        {/* If user ,home component else Login components*/}
-       {this.state.user ? (<Home/>) : (<Login/>)}
+        {/* If user ,home component else Login component*/}
+       {this.state.user ? (<Home />) : (<Login/>)}
+       {/* {this.state.user ? 
+       (<Router>
+         <nav>
+            <Link to='/' >Home</Link>
+            <Link to='/ProfilePage'>User Profile</Link>
+            <Link to='/Bookmarks'>Bookmarks</Link>
+         </nav>
+            <Route path='/' component={Home} exact />
+            <Route path='/ProfilePage' component={ProfilePage} />
+            <Route path='/Bookmarks' component={Bookmarks}/>
+       </Router>) : (<Login/>)} */}
       </div>
     )
   }
